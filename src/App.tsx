2f952593995c0556fb280fd7c3b93428c3e92244
Md. Ariful Islam <hmarifrazi@github.com>
2022-12-07 13:28:25 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2>Hello Vite react with redux type script</h2>
+    </div>
+  )
+}
+
+export default App
